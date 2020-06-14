5d4576eeaa58ee99919a7d4772cc8e554e81cc41
tetsu-upstr <bodomu@gmail.com>
2020-06-14 20:02:35 +0900

    html & css 追記

--- index.php
+++ index.php
@@ -3,11 +3,37 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>WORK OUT</title>
 </head>
 <body>
-  <h1>WORK OUT</h1>
-  
+  <header class="headerContainer">
+    <nav>
+      <ul>
+        <li>menu1</li> 
+        <li>menu2</li> 
+        <li>menu3</li> 
+      </ul>
+    </nav>
+  </header>
+
+  <div class="container">
+    <div class="sideContainer">
+      
+    </div>
+    <div class="mainContainer">
+      <h1>WORK OUT</h1>
+      <div class="viewContainer"></div>
+      <div class="menuContainer">
+          <ul>
+            <li>menu1</li>
+            <li>menu2</li>
+            <li>menu3</li>
+          </ul>
+      </div>
+
+    </div>
+  </div>
   
 </body>
 </html>
