e6d3c9baf3f0c253bad504e71afc0abe41157741
tetsu-upstr <bodomu@gmail.com>
2020-06-15 14:47:45 +0900

    timer css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,24 @@ const totalTime = 4000;
 const oldTime = Date.now();
 
 const timerId = setInterval(() => {
-  const currentTime = Date.now();
-  const diff = currentTime - oldTime;
+    const currentTime = Date.now();
+    const diff = currentTime - oldTime;
 
-  // 残り時間（ミリ秒）
-  const remainMSec = totalTime - diff;
+    // 残り時間（ミリ秒）
+    const remainMSec = totalTime - diff;
 
-  // ミリ秒を秒に変換
-  const remainSec = Math.ceil(remainMSec / 1000);
+    // ミリ秒を秒に変換
+    const remainSec = Math.ceil(remainMSec / 1000);
 
-  let label = `${remainSec}`;
+    let label = `${remainSec}`;
 
-  // 0秒以下になったらタイマー終了
-  if (remainSec <= 0) {
-    clearInterval(timerId);
+    // 0秒以下になったらタイマー終了
+    if (remainSec <= 0) {
+      clearInterval(timerId);
 
-    label = 'START';
-  }
+      label = 'START';
+    }
 
-  // 画面に表示する
-  document.getElementById('timer').innerHTML = label;
+    // 画面に表示する
+    document.getElementById('timer').innerHTML = label;
 }, 1000);
